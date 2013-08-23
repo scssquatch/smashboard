fef1c0e30d8e42a8402d823e789cd13db2e0288f
Aaron Baker <aaron.baker@neo.com>
2013-08-23 09:53:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smashboard::Application.config.secret_key_base = 'c18eca02d987da4640ad9b07d70e6cbca25f06d03fb3e9042b208ad82bdbc0d9b88a4c750d7e8210ea826f9beb6bf4cf5913f705fe8aab3b5d202a089bf03834'
