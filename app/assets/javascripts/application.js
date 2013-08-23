f239a1bb8d25d02124f91cf6de01173e6adb5e3c
Aaron Baker <aaron.baker@neo.com>
2013-08-23 15:59:07 -0400

    remove turbo links, import some old js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,27 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(function() {
+  $('body, header').noisy ({
+      'intensity' : 1,
+      'size' : '30',
+      'opacity' : 0.044,
+      'fallback' : '',
+      'monochrome' : false
+  }).css('background-color', '#2b2c2e');
+
+  $('.projects').noisy ({
+      'intensity' : 1,
+      'size' : '30',
+      'opacity' : 0.03,
+      'fallback' : '',
+      'monochrome' : false
+  }).css('background-color', '#272729');
+
+});
+
+function toClass (e) {
+  return '.' + e;
+}
